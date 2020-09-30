b02baf1e8bd814465eb16f762ef7c72c068bf73c
Kristian Joos <kjoos22@gmail.com>
2020-09-30 21:36:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,8 @@ class CashRegister
     if @discount == 0
       @total = @total
     else
-     "After the discount, the total comes to #{@total -= @total * (@discount / 100.0)}"
+     "After the discount, the total comes to
+      $#{@total -= @total * (@discount / 100.0).to_i}"
     end
   end
   
