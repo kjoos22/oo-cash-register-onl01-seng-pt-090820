175f3e620d29081d9b5dc5496607e313025e5e2d
Kristian Joos <kjoos22@gmail.com>
2020-09-30 21:26:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ class CashRegister
   attr_accessor :total, :discount, :last_transaction
   
   def initialize(discount = 0)
-    @total = 0
+    @total = 0.0
     @discount = discount
   end
   
@@ -11,7 +11,7 @@ class CashRegister
   end
   
   def apply_discount
-    
+    @total = 
   end
   
 end
