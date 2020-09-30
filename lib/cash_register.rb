fa13074caf9315ee3a5608519658ccb4060cfb71
Kristian Joos <kjoos22@gmail.com>
2020-09-30 21:23:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,5 @@
 class CashRegister
-  attr_accessor :total, :employee_discount :last_transaction
+  attr_accessor :total, :employee_discount, :last_transaction
   
   def initialize(employee_discount = 0)
     @total = 0
